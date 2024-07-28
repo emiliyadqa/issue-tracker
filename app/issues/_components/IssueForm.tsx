2896b4b547e33689739e8f795f25cdbd9f4c22ae
Emiliya Dobrogorska <emiliyadqa@gmail.com>
2024-07-28 15:24:23 -0700

    Implement UI for Update issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -40,7 +40,8 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
   const onSubmit = handleSubmit(async (data) => {
     try {
       setSubmitting(true);
-      await axios.post("/api/issues", data);
+      if (issue) await axios.patch("/api/issues/" + issue.id, data);
+      else await axios.post("/api/issues", data);
       router.push("/issues");
     } catch (error) {
       setSubmitting(false);
@@ -76,7 +77,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
         ></Controller>
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
         <Button disabled={isSubmitting}>
-          Add New Issue
+          {issue ? "Update Issue" : "Add New Issue"}
           {isSubmitting && <Spinner />}
         </Button>
       </form>
